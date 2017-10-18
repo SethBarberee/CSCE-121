d8170467fed50723bd64c66326f0fdafb346752c
Seth Barberee <seth.barberee@gmail.com>
2017-10-18 11:40:04 -0500

    update to recent stuff in lab

--- HW7/hw7pr1.cpp
+++ HW7/hw7pr1.cpp
@@ -1,47 +1,94 @@
+#include "std_lib_facilities_4.h"
+
 class Place {
     private:
     string name;
     string address;
     string category;
-}
+
+    public:
+    string get_name(){return name;}
+    string get_address(){return address;}
+    string get_category(){return category;}
+    Place(string name, string address, string category);
+};
 
 Place :: Place(string nam, string addr, string cat) : name(nam), address(addr), category(cat){}
 
 class School : public Place{
     private:
     int grade_level;
-}
+
+    public:
+    int get_grade_level(){return grade_level;}
+    void set_grade_level(int grade){grade_level = grade;}
+    School(string name, string address, string category, int grade_level);
+    School(string name, string address, string category);
+};
 
 School :: School(string nam, string addr, string cat, int grade) : Place(nam, addr, cat),  grade_level(grade){}
+School :: School(string nam, string addr, string cat) : Place(nam, addr, cat){}
+
+class High_School : public Place{
+    private:
+    string certification;
+
+    public:
+    string get_certification(){return certification;}
+    High_School(string name, string address, string category, certification);
+};
+
+High_School :: High_School(string nam, string addr, string cat, string cert) : School(nam, addr, cat), certification(cert){
+    set_grade_level(12);
+}
+
+class College : public Place{
+    private:
+    string degrees;
+
+    public:
+    string get_degrees(){return degrees;}
+    College(string name, string address, string category, string degrees);
+}
+
+College :: College(string nam, string addr, string cat, string degree) : School(nam, addr, cat), degrees(degree){
+    set_grade_level(16);
+}
 
 class Park : public Place {
     private:
     int hours_open;
     bool has_pool;
-}
+
+    public:
+    int get_hours_open(){return hours_open;}
+    bool get_has_pool(){return has_pool;}
+    Park(string name, string address, string category, int hours_open, bool has_pool);
+};
 
 Park :: Park(string nam, string addr, string cat, int hours, bool pool) : Place(nam, addr, cat), hours_open(hours), has_pool(pool){}
 
 class Hospital : public Place {
     private:
     int num_doctors;
-}
 
-Hospital :: Hospital(string nam, string addr, string cat, int docs) : Place(nam, addr, cat), num_doctors(doc){}
+    public:
+    int get_num_doctors(){return num_doctors;}
+    Hospital(string name, string address, string category, int num_doctors);
+};
 
-class Retail_shop : public Place {
+Hospital :: Hospital(string nam, string addr, string cat, int docs) : Place(nam, addr, cat), num_doctors(docs){}
+
+class Clinic : public Hospital {
     private:
-    string items;
-}
+    string surgeries;
 
-Retail_shop :: Retail_shop(string nam, string addr, string cat, string item) : Place(nam, addr, cat), items(item){}
+    public:
+    string get_surgeries(){return surgeries;}
+    Clinic(string name, string address, string category, int num_doctors, string surgeries);
+};
 
-class Mall : public Place {
-    private:
-    int num_shops;
-}
-
-Mall :: Mall(string nam, string addr, string cat, int nums) : Place(nam, addr, cat), num_shops(nums){}
+Clinic :: Clinic(string nam, string addr, string cat, int docs, string sur) : Hospital(nam, addr, cat, docs), surgeries(sur){}
 
 class Stadium : public Place {
     private:
@@ -50,38 +97,41 @@ class Stadium : public Place {
     
     public:
     void set_roof(bool roof){has_roof = roof;}
-}
+    bool get_has_roof(){return has_roof;}
+    string get_team(){return team;}
+    Stadium(string name, string address, string category, string team, bool has_roof);
+    Stadium(string name, string address, string category); // Lets child classes use the Place constructor
+};
 
 Stadium :: Stadium(string nam, string addr, string cat, string tem, bool roof) : Place(nam, addr, cat), team(tem), has_roof(roof){}
-
-class Restaraunt : public Place {
-    private:
-    string food_style;
-}
-
-Restaraunt :: Restaraunt(string nam, string addr, string cat, string style) : Place(nam, addr, cat), food_style(style){}
-
-class Insurance : public Place {
-    private:
-    string insurance_style;
-}
-
-Insurance :: Insurance(string nam, string addr, string cat, string style) : Place(nam, addr, cat), style(insurance_style){}
+Stadium :: Stadium(string nam, string addr, string cat) : Place(nam,addr,cat){} // Pass this constructor to Place class
 
 class Arena : public Stadium {
     private:
     string arena_type;
-}
 
-Arena :: Arena(string nam, string addr, string cat, string type) : Place(nam, addr, cat), arena_type(type){
-    Stadium :: Stadium.set_roof(true);
+    public:
+    string get_arena_type(){return arena_type;}
+    Arena(string name, string address, string category, string arena_type);
+};
+
+Arena :: Arena(string nam, string addr, string cat, string type) : Stadium(nam, addr, cat), arena_type(type){
+    set_roof(true);
 }
 
 class Field : public Stadium {
     private:
     string field_type;
+
+    public:
+    string get_field_type(){return field_type;}
+    Field(string name, string address, string category, string field_type);
+};
+
+Field :: Field(string nam, string addr, string cat, string type) : Stadium(nam, addr, cat), field_type(type){
+    set_roof(false);
 }
 
-Field :: Field(string nam, string addr, string cat, string type) : Place(nam, addr, cat), field_type(type){
-    Stadium :: Stadium.set_roof(false);
+int main(){
+    return 0;
 }
