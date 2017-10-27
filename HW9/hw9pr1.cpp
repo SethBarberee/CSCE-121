80409af899b82b8aa8ab51ae80b0c06eb08299d0
Seth Barberee <seth.barberee@gmail.com>
2017-10-27 10:01:49 -0500

    started HW9

---
+++ HW9/hw9pr1.cpp
@@ -0,0 +1,15 @@
+class Shape{
+    private:
+    double x,y;
+    double distance;
+    int num_vertex;
+};
+class Equilateral_Triangle : public Shape{};
+class Square : public Shape{};
+class Circle : public Shape{};
+class Right_Triangle{};
+class Rectangle{};
+class Regular_Pentagon{};
+
+
+int main(){}
