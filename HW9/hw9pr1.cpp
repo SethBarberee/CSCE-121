3e2bd5e6f1863546f630f3a4432dbbffc8277ef9
Seth Barberee <seth.barberee@gmail.com>
2017-11-01 09:41:06 -0500

    added Points and worked on pointers.... Pointers are killing me

--- HW9/hw9pr1.cpp
+++ HW9/hw9pr1.cpp
@@ -5,11 +5,23 @@
 #include "std_lib_facilities_4.h"
 double pi = 3.14159;
 
+struct Point{
+    int Point_x,Point_y;
+    Point(int Point_x, int Point_y);
+    int get_x(){return Point_x;}
+    int get_y(){return Point_y;}
+};
+Point :: Point(int x, int y) : Point_x(x), Point_y(y){}
+ostream& operator <<(ostream& out, Point& a){
+    out << "(" << a.get_x() << ", " << a.get_y() << ")";
+}
+
 class Shape{
     private:
     int num_sides;
     double distance;
     char type;
+    vector<Point> Points;
 
     protected:
     int x, y;
@@ -20,19 +32,29 @@ class Shape{
     virtual void calc_area(){};
 
     public:
-    void set_num_sides(int sides){num_sides = sides;};
-    int get_num_sides(){return num_sides;};
-    void set_type(char shape_type){type = shape_type;};
-    char get_type(){return type;};
-    double get_perimeter(){return perimeter;};
+    void set_num_sides(int sides){num_sides = sides;}
+    int get_num_sides(){return num_sides;}
+    void set_type(char shape_type){type = shape_type;}
+    char get_type(){return type;}
+    double get_perimeter(){return perimeter;}
     double get_area(){return area;};
+    void add_point(Point a){
+        Points.push_back(a);
+    }
+    void print_points(){
+        for (int i = 0; i < Points.size(); i++){
+            cout << Points[i] << endl;
+        }
+    }
+    vector<Point> get_points(){return Points;}
+    Point get_point(int id){return Points[id];}
     Shape(int x, int y, double distance);
     Shape(int x, int y);
 };
 Shape :: Shape(int x1, int y1, double vertex_distance) : x(x1), y(y1), distance(vertex_distance){}
 Shape :: Shape(int x1, int y1) : x(x1), y(y1){}
 ostream& operator <<(ostream& out, Shape& a){
-    out << "Type: " << a.get_type() << endl << "Num_Sides: " << a.get_num_sides() << endl << "Area: " << a.get_area() << endl << "Perimeter: " << a.get_perimeter() << endl;
+    out << "Type: " << a.get_type() << endl << "Num_Sides: " << a.get_num_sides() << endl << "Area: " << a.get_area() << endl << "Perimeter: " << a.get_perimeter() << endl << "Points: " << endl;
     return out;
 }
 
@@ -61,9 +83,21 @@ class Square : public Shape{
     Square(int x, int y, double distance);
 };
 Square :: Square(int x1, int y1, double vertex_distance) : Shape(x1, y1, vertex_distance){
+    Point Point_1 = Point(0,0);
+    Point Point_2 = Point(0,0);
+    Point Point_3 = Point(0,0);
+    Point Point_4 = Point(0,0);
     set_type('S');
     set_num_sides(4);
-    side_length = (vertex_distance * 2) / sqrt(2);
+    side_length = (vertex_distance * 2);
+    Point_1 = Point(x1-vertex_distance,y1+vertex_distance);
+    Point_2 = Point(x1+vertex_distance,y1+vertex_distance);
+    Point_3 = Point(x1-vertex_distance,y1-vertex_distance);
+    Point_4 = Point(x1+vertex_distance,y1-vertex_distance);
+    add_point(Point_1);
+    add_point(Point_2);
+    add_point(Point_3);
+    add_point(Point_4);
     calc_area();
     calc_perimeter();
 }
@@ -82,8 +116,11 @@ class Circle : public Shape{
     Circle(int x, int y, int radius);
 };
 Circle :: Circle(int x1, int y1, int rad) : Shape(x1, y1), radius(rad){
+    Point Point_1 = Point(0,0);
     set_type('C');
     set_num_sides(1);
+    Point_1 = Point(x1, y1);
+    add_point(Point_1);
     calc_area();
     calc_perimeter();
 }
@@ -104,11 +141,20 @@ class Right_Triangle : public Shape{
     Right_Triangle(int x, int y, int x2, int y2, int x3, int y3);
 };
 Right_Triangle :: Right_Triangle(int x_1, int y_1, int x_2, int y_2, int x_3, int y_3) : Shape(x_1, y_1), x2(x_2), y2(y_2), x3(x_3), y3(y_3){
+    Point Point_1 = Point(0,0);
+    Point Point_2 = Point(0,0);
+    Point Point_3 = Point(0,0);
     set_type('T');
     set_num_sides(3);
     side_a = sqrt(((x2-x) * (x2-x)) + ((y2 -y) * (y2-y)));
     side_b = sqrt(((x3-x) * (x3-x)) + ((y3 -y) * (y3-y)));
     side_c = sqrt((side_a * side_a) + (side_b * side_b));
+    Point_1 = Point(x_1, y_1);
+    Point_2 = Point(x_2, y_2);
+    Point_3 = Point(x_3, y_3);
+    add_point(Point_1);
+    add_point(Point_2);
+    add_point(Point_3);
     calc_area();
     calc_perimeter();
 }
@@ -128,8 +174,20 @@ class Rectangle : public Shape{
     Rectangle(int x, int y, int width, int height);
 };
 Rectangle :: Rectangle(int x1, int y1, int width_r, int height_r) : Shape(x1, y1), width(width_r), height(height_r){
+    Point Point_1 = Point(0,0);
+    Point Point_2 = Point(0,0);
+    Point Point_3 = Point(0,0);
+    Point Point_4 = Point(0,0);
     set_type('R');
     set_num_sides(4);
+    Point_1 = Point(x1, y1);
+    Point_2 = Point(x1+width_r, y1);
+    Point_3 = Point(x1+width_r, y1-height_r);
+    Point_4 = Point(x1, y1-height_r);
+    add_point(Point_1);
+    add_point(Point_2);
+    add_point(Point_3);
+    add_point(Point_4);
     calc_area();
     calc_perimeter();
 }
@@ -187,57 +245,57 @@ void import_file(string input, vector<Shape>& a){
             case 'T':
                 // import is a right triangle
                 ist >> x >> y >> x2 >> y2 >> x3 >> y3;
+                cout << "Creating right triangle" << endl;
                 Shape_T = new Right_Triangle(x, y, x2, y2, x3, y3);
                 a.push_back(*Shape_T); // push the right triangle object to the vector
-                delete[] Shape_T;
                 shape_type = ' ';
                 break;
             case 'R':
                 // import is a rectangle
                 ist >> x >> y >> width >> height;
+                cout << "Creating rectangle" << endl;
                 Shape_R = new Rectangle(x, y, width, height);
                 a.push_back(*Shape_R); // push the rectangle object to the vector
-                delete[] Shape_R;
                 shape_type = ' ';
                 break;
             case 'E':
                 // import is a equilateral triangle
                 ist >> x >> y >> vertex_distance;
+                cout << "Creating equilateral triangle" << endl;
                 Shape_E = new Equilateral_Triangle(x, y, vertex_distance);
                 a.push_back(*Shape_E); // push the equilateral triangle object to the vector
-                delete[] Shape_E;
                 shape_type = ' ';
                 break;
             case 'S':
                 // import is a square
                 ist >> x >> y >> vertex_distance;
+                cout << "Creating a sqaure" << endl;
                 Shape_S = new Square(x, y, vertex_distance);
                 a.push_back(*Shape_S); // push the square object to the vector
-                delete[] Shape_S;
                 shape_type = ' ';
                 break;
             case 'P':
                 // import is a pentagon
                 ist >> x >> y >> vertex_distance;
+                cout << "Creating a pentagon" << endl;
                 Shape_P = new Regular_Pentagon(x, y, vertex_distance);
                 a.push_back(*Shape_P); // push the pentagon object to the vector
-                delete[] Shape_P;
                 shape_type = ' ';
                 break;
             case 'H':
                 // import is a hexagon
                 ist >> x >> y >> vertex_distance;
+                cout << "Creating a hexagon" << endl;
                 Shape_H = new Regular_Hexagon(x, y, vertex_distance);
                 a.push_back(*Shape_H); // push the hexagon object to the vector
-                delete[] Shape_H;
                 shape_type = ' ';
                 break;
             case 'C':
                 // import is a circle
                 ist >> x >> y >> rad;
+                cout << "Creating a circle" << endl;
                 Shape_C = new Circle(x, y, rad);
                 a.push_back(*Shape_C); // push the circle object to the vector
-                delete[] Shape_C;
                 shape_type = ' ';
                 break;
             default:
@@ -257,7 +315,9 @@ int main(){
         import_file(inputfile, Shapes);
 
         for(int i = 0; i < Shapes.size(); i++){
-            cout << Shapes[i] << endl; // output each shape info to the screen
+            cout << Shapes[i]; // output each shape info to the screen
+            Shapes[i].print_points();
+            cout << endl;
         }
     }
     catch(exception& e){
