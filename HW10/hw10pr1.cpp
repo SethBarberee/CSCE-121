6ff4bc87bd7a492544cbb9703823832ac74c7aaf
Seth Barberee <seth.barberee@gmail.com>
2017-11-15 09:43:40 -0600

    started on HW10

---
+++ HW10/hw10pr1.cpp
@@ -0,0 +1,21 @@
+// Name: Seth Barberee
+// Assignment: Homework 10 Problem 1
+// Purpose: Make a double linked list
+
+class List {
+    private:
+    int val;
+    List* first;
+    List* last;
+
+    public:
+    List(){};
+    List* insert(){};
+    List* insert_pos(int n){};
+    List* remove_pos(int n){};
+};
+
+ostream& operator<<(ostream& os, List& a){
+    // TODO add stuff to output
+    return os;
+}
