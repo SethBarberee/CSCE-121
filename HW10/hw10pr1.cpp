5781f36d87162e0f26845090c8bd0a4cf763ecbb
Seth Barberee <seth.barberee@gmail.com>
2017-11-17 10:01:39 -0600

    worked on HW10 more

--- HW10/hw10pr1.cpp
+++ HW10/hw10pr1.cpp
@@ -4,12 +4,10 @@
 
 struct Node {
     int val;
-    Node* first;
     Node* prev;
     Node* next;
-    Node* last;
     int get_val(){return val;}
-    Node(int d, Node* f = 0; Node* p = 0; Node* n = 0; Node* l = 0;) : val(d), first(f), prev(p), next(n), last(l) {}
+    Node(int d, Node* p = 0; Node* n = 0;) : val(d), prev(p), next(n){}
 };
 ostream& operator<<(ostream& os, Node& a){
     os << a.get_val();
@@ -20,19 +18,30 @@ class List {
     private:
     int size;
     Node* nodes;
+    Node* first;
+    Node* last;
     
     public:
-    List(){size = 0; nodes = nullptr;}
+    List(){size = 0; nodes = nullptr; first = nullptr; last = nullptr;}
     // TODO Copy Constructor
     // TODO Copy Assignment
     // TODO Destructor
+    int get_size(){return size;}
     Node* get_nodes{ return nodes;}
-    Node* insert(){}
+    Node* get_last{ return last;}
+    Node* insert(Node* a){
+        a->next = nullptr;
+        a->prev = get_last();
+        get_last()->next = a;
+        last = a;
+        size++; // Increase size on successful insert
+    }
     Node* insert_pos(int n){
         if(n <= size){
             for(int i = 0; i < n-1; i++){
                 // TODO loop through nodes to needed node
             }
+            size++; // Increase size on successful insert
         }
         else{
             return nullptr;
@@ -43,6 +52,7 @@ class List {
             for(int i = 0; i < n-1; i++){
                // TODO loop through nodes to needed node 
             }
+            size--; // Decrease size on successful removal
         }
         else{
             return nullptr;
@@ -52,6 +62,7 @@ class List {
 ostream& operator<<(ostream& os, List& a){
     // TODO loop through Nodes and cout them
     // os << Node
+    os << a.get_size();
     return os;
 }
 int main(){}
