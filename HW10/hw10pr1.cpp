0b93a162a99f267ea8316276f6763854bb4df1cd
Seth Barberee <seth.barberee@gmail.com>
2017-11-16 22:36:56 -0600

    changed to implement a Node Struct and List Class

--- HW10/hw10pr1.cpp
+++ HW10/hw10pr1.cpp
@@ -1,44 +1,57 @@
-// Name: Seth Barberee
+// Author: Seth Barberee
 // Assignment: Homework 10 Problem 1
 // Purpose: Make a double linked list
 
-#include "std_lib_facilities_4.h"
-
-class List {
-    private:
+struct Node {
     int val;
-    List* first;
-    List* last;
-
-    public:
+    Node* first;
+    Node* prev;
+    Node* next;
+    Node* last;
     int get_val(){return val;}
-    List(List* f = 0, List* l = 0): first(f), last(l){};
-    List* insert(List* p, List* n){
-        n->first = p->first;
-        p->last = n; // point to the newest last element
-        n->last = nullptr; // it's the last element
-        return n;
-    };
-    List* insert_pos(List* p, List* n, int r){
-        // TODO need to figure out size of list
-        // since code can be different depending on where
-        // we are inserting
-        //
-        // Assume it's in the middle
-        n->first = p->first;
-        n->last = p->last;
-        return n;
-    };
-    List* remove_pos(List* p, int n){};
+    Node(int d, Node* f = 0; Node* p = 0; Node* n = 0; Node* l = 0;) : val(d), first(f), prev(p), next(n), last(l) {}
 };
-
-ostream& operator<<(ostream& os, List& a){
-    // TODO add stuff to output
-    
+ostream& operator<<(ostream& os, Node& a){
+    os << a.get_val();
     return os;
 }
 
-int main(){
-    List* test_list;
-    cout << *test_list << endl;;
+class List {
+    private:
+    int size;
+    Node* nodes;
+    
+    public:
+    List(){size = 0; nodes = nullptr;}
+    // TODO Copy Constructor
+    // TODO Copy Assignment
+    // TODO Destructor
+    Node* get_nodes{ return nodes;}
+    Node* insert(){}
+    Node* insert_pos(int n){
+        if(n <= size){
+            for(int i = 0; i < n-1; i++){
+                // TODO loop through nodes to needed node
+            }
+        }
+        else{
+            return nullptr;
+        }
+    }
+    Node* remove_pos(int n){
+        if(n <= size){
+            for(int i = 0; i < n-1; i++){
+               // TODO loop through nodes to needed node 
+            }
+        }
+        else{
+            return nullptr;
+        }
+    }
+};
+ostream& operator<<(ostream& os, List& a){
+    // TODO loop through Nodes and cout them
+    // os << Node
+    return os;
 }
+int main(){}
