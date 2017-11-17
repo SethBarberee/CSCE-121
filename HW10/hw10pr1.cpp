2db803932f95518567ef4026ae71266118116455
Seth Barberee <seth.barberee@gmail.com>
2017-11-17 11:25:48 -0600

    compiles good and maybe it works?

--- HW10/hw10pr1.cpp
+++ HW10/hw10pr1.cpp
@@ -2,46 +2,65 @@
 // Assignment: Homework 10 Problem 1
 // Purpose: Make a double linked list
 
+#include "std_lib_facilities_4.h"
+
 struct Node {
     int val;
     Node* prev;
     Node* next;
-    int get_val(){return val;}
-    Node(int d, Node* p = 0; Node* n = 0;) : val(d), prev(p), next(n){}
+    int get_val() const {return val;}
+    Node(int d, Node* p = 0, Node* n = 0) : val(d), prev(p), next(n){};
 };
-ostream& operator<<(ostream& os, Node& a){
-    os << a.get_val();
-    return os;
+ostream& operator<<(ostream& os, const Node& a){
+    return os << a.get_val();
 }
 
 class List {
     private:
     int size;
-    Node* nodes;
+    Node* current_node;
     Node* first;
     Node* last;
     
     public:
-    List(){size = 0; nodes = nullptr; first = nullptr; last = nullptr;}
+    List(){size = 0; current_node = nullptr; first = nullptr; last = nullptr;};
     // TODO Copy Constructor
     // TODO Copy Assignment
     // TODO Destructor
-    int get_size(){return size;}
-    Node* get_nodes{ return nodes;}
-    Node* get_last{ return last;}
+    int get_size() const {return size;}
+    Node* get_current_node(){ return current_node;}
+    Node* get_last(){ return last;}
     Node* insert(Node* a){
-        a->next = nullptr;
-        a->prev = get_last();
-        get_last()->next = a;
-        last = a;
-        size++; // Increase size on successful insert
-    }
-    Node* insert_pos(int n){
-        if(n <= size){
-            for(int i = 0; i < n-1; i++){
-                // TODO loop through nodes to needed node
-            }
+        if(size > 0){ // check if it's not empty
+            a->next = nullptr;
+            a->prev = get_last();
+            this->get_last()->next = a;
+            last = a;
+            current_node = a;
             size++; // Increase size on successful insert
+            return a;
+        }
+        else{
+            // List was empty but now, we're adding it's first node
+            a->next = nullptr;
+            a->prev = nullptr;
+            first = a;
+            last = a;
+            size++; // Increase size on successful insert
+            current_node = a;
+            return a;
+        }
+    }
+    Node* insert_pos(Node*a, int n){
+        if(n <= size){
+            for(int i = 1; i < n; i++){
+                this->current_node = this->current_node->next;
+            }
+            this->current_node->next->prev = a;
+            this->current_node->next = a;
+            size++; // Increase size on successful insert
+            current_node = a;
+            return a;
         }
         else{
             return nullptr;
@@ -49,20 +68,24 @@ class List {
     }
     Node* remove_pos(int n){
         if(n <= size){
-            for(int i = 0; i < n-1; i++){
-               // TODO loop through nodes to needed node 
+            for(int i = 1; i < n; i++){
+                this->current_node = this->current_node->next; 
             }
+            this->current_node->prev->next = this->current_node->next;
+            this->current_node->next->prev = this->current_node->prev;
             size--; // Decrease size on successful removal
+            return this->current_node;
         }
         else{
             return nullptr;
         }
     }
 };
-ostream& operator<<(ostream& os, List& a){
+ostream& operator<<(ostream& os, const List& a){
     // TODO loop through Nodes and cout them
     // os << Node
-    os << a.get_size();
-    return os;
+    return os << a.get_size();
 }
-int main(){}
+int main(){
+    List* demo = new List();
+}
