f9a031a5f04764dd13d4ed688683d2756294b499
Seth Barberee <seth.barberee@gmail.com>
2017-11-29 12:16:52 -0600

    still working on it

--- HW11/hw11pr1.cpp
+++ HW11/hw11pr1.cpp
@@ -3,7 +3,7 @@
 template<class Iter1, class Iter2>
 Iter2 my_copy(Iter1 f1, Iter1 e1, Iter2 f2){
     if(f1 == e1){
-        return *f2;
+        return f2;
     }
     else{
         // TODO for loop to copy
@@ -13,7 +13,7 @@ Iter2 my_copy(Iter1 f1, Iter1 e1, Iter2 f2){
 template<class Iter1, class T>
 void add_each(Iter1 f1, Iter1 e1, T add){
     while (f1 != e1){
-        f1 = f1 + add;
+        *f1 = *f1 + add;
         ++f1;
     }
 }
@@ -23,16 +23,16 @@ int main(){
     vector<int> vector_test = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     list<int> list_test = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    int array_me[] = copy(array_test.begin(), array_test.end(), array_me.begin());
+    int array_me[] = copy(array_test, array_test+sizeof(array_test)/sizeof(*array_test), array_me);
     vector<int> vector_me = copy(vector_test.begin(), vector_test.end(), vector_me.begin());
-    list<int> list_me =  copy(list_test.begin(), list_test.begin(), list_me.begin());
+    list<int> list_me =  copy(list_test.begin(), list_test.end(), list_me.begin());
 
-    add_each(array_test.begin(), array_test.end(), 2);
+    add_each(array_test, array_test+sizeof(array_test)/sizeof(*array_test), 2);
     add_each(vector_test.begin(), vector_test.end(), 3);
     add_each(list_test.begin(), list_test.end(), 5);
 
-    my_copy(array_me.begin(), array_me.end(), vector_me.begin());
-    my_copy(list_me.begin(), list_me.end(), array_me.begin());
+    my_copy(array_me, array_me+sizeof(array_me)/sizeof(*array_me), vector_me.begin());
+    my_copy(list_me.begin(), list_me.end(), array_me);
 
     find(vector_test.begin(), vector_test.end(), 3);
     find(list_test.begin(), list_test.end(), 27);
--- HW11/hw11pr2.cpp
+++ HW11/hw11pr2.cpp
@@ -9,6 +9,16 @@ struct Record {
     Record(int units_sold, double unit_price);
 };
 
+// TODO work on this!!
+template<class T> struct total_func{
+    T total;
+    Record a;
+    total_func(T& v, const Record& r) : total(v), a(r) {}
+    bool operator()(double v, const Record& r) const {
+        return v + r.unit_price * r.units_sold;
+    }
+};
+
 Record :: Record(int unit, double price) : units_sold(unit), unit_price(price){}
 
 double total(double v, const Record& r){
@@ -36,7 +46,7 @@ int main(){
     // Function object as an argument to accumulate
     // I.E. multiplies<double>()
     cout << "Accumulate using a function object..." << endl;
-    //accumulate(Records.begin(), Records.end(), );
+    //accumulate(Records.begin(), Records.end(), total_func());
     // Lamda function as an argument to accumulate
     cout << "Accumulate using a lamda function..." << endl;
     cout << accumulate(Records.begin(), Records.end(),0.0,[](double v, const Record& r){return v + r.unit_price*r.units_sold;}) << endl;
